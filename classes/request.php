14bac50fe7411c73a06039e868d21fdc8bddb37d
nalla <allan.korts@khk.ee>
2013-02-21 12:21:52 +0200

    fixed bug with request.php

--- classes/request.php
+++ classes/request.php
@@ -16,12 +16,11 @@ class request
 	function __construct()
 	{
 		if (isset($_SERVER['PATH_INFO'])){
-
 			if( $path_info = explode('/', $_SERVER['PATH_INFO']) ) {
 				array_shift($path_info);
 				$this->controller = isset($path_info[0]) ? array_shift($path_info) : DEFAULT_CONTROLLER;
 				$this->action     = isset($path_info[0]) ? array_shift($path_info) : 'index';
-				$this->params     = isset($path_info) ? $path_info : null;
+				$this->params     = isset($path_info[0]) ? $path_info : null;
 			}
 		}
 	}
