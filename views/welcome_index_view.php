6ba1e8ea459ab88c253754337f297c33be814c0f
Karmen <karmen.kukk@khk.ee>
2013-05-02 22:01:48 +0300

    Fix 5th <li> in welcome_index_view.php

--- views/welcome_index_view.php
+++ views/welcome_index_view.php
@@ -3,5 +3,5 @@
 	<li>2. Rename welcome controller enda põhikontrolleri nimeliseks (ära unusta classi nime muuta).</li>
 	<li>3. Muuda config.php-s muutujad ära.</li>
 	<li>4. Kohanda master_view.php failis pealkiri ja projekti nimi.</li>
-	<li>5. Loo enda põhikontrolleri jaoks vaatefail (näiteks views/products_view.php).</li>
+	<li>5. Loo enda põhikontrolleri jaoks vaatefail (näiteks views/products_index_view.php).</li>
 </ul>
