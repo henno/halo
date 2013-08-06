09b91c7ab0bf2d07f03574286944ecf43512b2ae
henno <henno.taht@diara.ee>
2013-08-06 19:08:10 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', 'toor');
-define('DATABASE_DATABASE', 'halso');
-define('DEFAULT_CONTROLLER', 'welcome');
-define('BASE_URL', '/halo/');
-define('ASSETS_URL', '/halo/assets/');
