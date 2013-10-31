27dcbce3a032e6fdb9f2936d6b618bae078f4309
Henno Taht <henno.taht@khk.ee>
2013-10-31 16:37:41 +0200

    Change default database hostname from localhost to 127.0.0.1 (speed boost on Windows Vista and later)

--- config.sample.php
+++ config.sample.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE_HOSTNAME', 'localhost');
+define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
 define('DATABASE_DATABASE', PROJECT_NAME);
