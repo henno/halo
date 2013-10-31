da5a2dddadae36f13bd917e0811c3286baf5aaa9
Henno Täht <henno.taht@gmail.com>
2013-10-31 16:33:53 +0200

    Make master template dynamic (add view showing)

--- views/welcome/welcome_index.php
+++ views/welcome/welcome_index.php
@@ -1,5 +1,1 @@
-<ul>
-	<li>1. Rename welcome controller enda põhikontrolleri nimeliseks (ära unusta classi nime muuta).</li>
-	<li>3. Kohanda views/templates/master_template.php failis pealkiri ja projekti nimi.</li>
-	<li>4. Loo enda põhikontrolleri jaoks vaatefail (näiteks views/products_index_view.php).</li>
-</ul>
+palceholder for welcome view
