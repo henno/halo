c817ba227ac2cc699a4d044602e68c56adfa9b7b
Henno Täht <henno.taht@gmail.com>
2015-07-28 14:37:14 +0300

    Move framework scaffolding code from config/index.php to controllers/admin.php

--- controllers/admin.php
+++ controllers/admin.php
@@ -3,7 +3,65 @@
 class admin extends Controller
 {
 
-    function index()
+    function index(){}
+
+    function POST_index()
     {
+
+        // Check if the controller's table already exists in the database
+        $table_names_are_singular = true;
+        $name_plural = $_POST['name_plural'];
+        $name_singular = $_POST['name_singular'];
+        $table_name = $table_names_are_singular ? $name_singular : $name_plural;
+
+        if (q("SHOW TABLES LIKE '$table_name'")) {
+
+            // Show error
+            echo '<div class="alert alert-danger">' . "The table $name_plural already existed. Aborting." . '</div>';
+
+        } else {
+
+            // Add table to database
+            $name_plural = @mysql_real_escape_string($name_plural);
+            q("CREATE TABLE `{$table_name}` (
+             `{$name_singular}_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Autocreated',
+             `{$name_singular}_name` varchar(50) NOT NULL COMMENT 'Autocreated',
+             PRIMARY KEY (`{$name_singular}_id`)
+           ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
+            echo '<div class="alert alert-success">' . "The table $table_name was created ." . '</div>';
+
+            // Add controller from template (substituting module for controller's name)
+            $content = file_get_contents('config/controller_template.php');
+            $content = str_replace('module', $name_singular, $content);
+            $fp = fopen("controllers/$name_plural.php", "wb");
+            fwrite($fp, $content);
+            fclose($fp);
+
+            /** Add views **/
+            $views = ['index', 'view', 'edit'];
+
+            // Create views directory
+            $dirname = "views/$name_plural";
+            if (!is_dir($dirname)) {
+                mkdir($dirname, 0755);
+            }
+
+            // Create each view
+            foreach ($views as $view) {
+                $content = file_get_contents("config/view_{$view}_template.php");
+                $content = str_replace('module', $name_singular, $content);
+                $fp = fopen("views/$name_plural/{$name_plural}_$view.php", "wb");
+                fwrite($fp, $content);
+                fclose($fp);
+            }
+
+            exec("git add controllers/*");
+            exec("git add views/*");
+            exec("chmod -R 777 *");
+
+
+        }
+
     }
+
 }
--- views/admin/admin_index.php
+++ views/admin/admin_index.php
@@ -1,5 +1,34 @@
-<div class="page-header">
-    <h1>Admin area
-        <small>Put your password protected stuff here</small>
-    </h1>
+<div class="container">
+    <h1>Halo configuration page</h1>
+
+    <p class="help-block">
+        This page allows you automatically create a new controller which includes
+    <ul>
+        <li>a database table,</li>
+        <li>a controller with 3 actions (index, view, edit)</li>
+        <li>views (index, view, edit)</li>
+    </ul>
+    </p>
+    <h3>New controller</h3>
+
+    <p class="help-block">Please input new controller name in singular and plural:</p>
+
+    <form method="post">
+
+        <div class="input-group">
+            <span class="input-group-addon">Controller name singular</span>
+            <input type="text" class="form-control" placeholder="user" name="name_singular">
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon">Controller name plural</span>
+            <input type="text" class="form-control" placeholder="users" name="name_plural">
+        </div>
+        <p>
+
+        <div class="pull-right">
+            <button class="btn btn-primary" type="submit">Add</button>
+        </div>
+        </p>
+    </form>
+
 </div>
