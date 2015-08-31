8b377e72c5579ee21df8bf18ceb706d5cbb69a02
Henno Täht <henno.taht@gmail.com>
2015-08-31 14:03:19 +0300

    Resolve issue with logout not working for login-only type websites

--- controllers/logout.php
+++ controllers/logout.php
@@ -8,9 +8,12 @@
  */
 class logout extends Controller
 {
+    public $requires_auth = false;
+
     function index()
     {
         session_destroy();
         header('Location: ' . BASE_URL);
+        exit();
     }
 } 
