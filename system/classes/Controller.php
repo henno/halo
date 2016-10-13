7c21ecd869dee391ce39cf51fb75875ddaa299b1
Henno Täht <henno.taht@gmail.com>
2016-10-13 13:27:29 +0300

    Follow-up to previous commit (add missing $supported_languages)

--- system/classes/Controller.php
+++ system/classes/Controller.php
@@ -7,6 +7,8 @@ class Controller
 
     function render($template)
     {
+        global $supported_languages;
+
         // Make controller variables available to view
         extract(get_object_vars($this));
 
