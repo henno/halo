8253312ad21228c2a6b2883086b86666d9d09336
henno <henno.taht@gmail.com>
2017-02-06 11:34:12 +0200

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -5,6 +5,3 @@ require 'system/config.default.php';
 
 // Load local customizations
 $cfg['DATABASE_DATABASE'] = 'halo';
-
-
-die('Run composer install and remove line ' . __LINE__ . ' from config.php');
